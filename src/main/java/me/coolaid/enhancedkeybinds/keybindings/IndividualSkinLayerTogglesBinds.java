f2672b4f45949c3698900b58b36e620bf4bd4b3f
CoolAid48 <atgunsel@gmail.com>
2026-07-22 18:07:05 -0400

    Port 4.0.0 to 26.1.x

--- src/main/java/me/coolaid/enhancedkeybinds/keybindings/IndividualSkinLayerTogglesBinds.java
+++ src/main/java/me/coolaid/enhancedkeybinds/keybindings/IndividualSkinLayerTogglesBinds.java
@@ -3,7 +3,7 @@ package me.coolaid.enhancedkeybinds.keybindings;
 import com.mojang.blaze3d.platform.InputConstants;
 import me.coolaid.enhancedkeybinds.config.EnhancedKeybindsConfig;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.PlayerModelPart;
@@ -51,7 +51,7 @@ public final class IndividualSkinLayerTogglesBinds {
             return;
         }
 
-        KeyMapping keyMapping = KeyBindingHelper.registerKeyBinding(
+        KeyMapping keyMapping = KeyMappingHelper.registerKeyMapping(
                 new KeyMapping(translationKey, InputConstants.UNKNOWN.getValue(), RegisterCategories.SKIN_CUSTOMIZATION)
         );
         BINDINGS.add(new SkinLayerBinding(keyMapping, modelPart));
